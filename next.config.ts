efa451bca251efda5336dbfc3eb7c80e991fe3ed
Piotr Nowak <piotr.nowak@creativestyle.pl>
2025-07-12 20:49:07 +0200

    change path

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  trailingSlash: true,
+  skipTrailingSlashRedirect: true,
+  distDir: 'dist',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/ravedron' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/ravedron' : '',
+  env: {
+    PUBLIC_URL: process.env.NODE_ENV === 'production' ? '/ravedron' : ''
+  }
 };
 
 export default nextConfig;
